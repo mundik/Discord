bc3f0f70a64462ec5bec514fb74ca50e7cb20592
Mundik <jankrumpholc99@gmail.com>
2021-12-22 15:02:29 +0100

    Added guide

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import Workout
 import Anime
 import System
 import Notes
+import Guide
 import time as Time
 
 TOKEN = 'NzMzOTE5NTQ2MTg0MDQwNTA5.XxKJ1w.fkLthMofrT3g7DSGBWB59BGrYKo'
@@ -30,24 +31,26 @@ async def workout(ctx, *args):
     try:
         func = args[0]
     except IndexError:
-        await ctx.send("Available functions: sub, status")
+        await ctx.send(Guide.workout_wrong_function)
         return
     if func == "sub":
-        if len(args) != 3:
-            await ctx.send("Type and number needed")
+        if len(args) < 3:
+            await ctx.send(Guide.workout_sub_too_few)
+        elif len(args) > 3:
+            await ctx.send(Guide.workout_sub_too_many)
         else:
             option = args[1]
             num = args[2]
             remain = Workout.substract(option, num)
             if remain == "":
-                await ctx.send("Wrong type of operation.")
+                await ctx.send(Guide.workout_sub_wrong_operation)
             else:
                 await ctx.send(f"Substracted {num} points from {option}.\nNew value: {remain}")
     elif func == "status" or func == "stat":
         data = Workout.status()
         await ctx.send(f"Remaining workout points:\n {data}")
     else:
-        await ctx.send("Wrong function(available: status,sub)")
+        await ctx.send(Guide.workout_wrong_function)
 
 
 @bot.command()
@@ -65,22 +68,20 @@ async def anime(ctx, *args):
     try:
         func = args[0]
     except IndexError:
-        await ctx.send("Available functions: watched, add, add_going, finished, status, waiting, update")
+        await ctx.send(Guide.anime_main)
         return
     if func == "watched" or func == "w":
-        data = Anime.watched(args[1:-1], args[-1]) if len(args) >= 3 else \
-            "Missing parameters (syntax: watched Name Num_of_ep)"
+        data = Anime.watched(args[1:-1], args[-1]) if len(args) >= 3 else Guide.anime_watched
         await ctx.send(data)
     elif func == "add":
-        data = Anime.new_anime(args[1:-2], args[-2], args[-1]) if len(args) >= 4 else \
-            "Missing parameters (syntax: add Name Act_ep Num_of_ep)"
+        data = Anime.new_anime(args[1:-2], args[-2], args[-1]) if len(args) >= 4 else Guide.anime_add
         await ctx.send(data)
     elif func == "add_going":
-        data = Anime.new_anime_going(args[1:-4], args[-4], args[-3], args[-2], args[-1]) if len(args) >= 6 \
-            else "Missing parameters (syntax: add Name Act_ep Num_ep Day Last_updated, Update_hour)"
+        data = Anime.new_anime_going(args[1:-4], args[-4], args[-3], args[-2], args[-1]) if len(args) >= 6 else \
+            Guide.anime_add_going
         await ctx.send(data)
     elif func == "finished" or func == "f":
-        data = Anime.finished(args[1:]) if len(args) >= 2 else "Missing parameters (syntax: finished Name)"
+        data = Anime.finished(args[1:]) if len(args) >= 2 else Guide.anime_finished
         await ctx.send(data)
     elif func == "status" or func == "stat":
         data = Anime.status()
@@ -101,15 +102,13 @@ async def anime(ctx, *args):
         data = "Nothing to update" if data == "" else data
         await ctx.send(data)
     elif func == "transfer" or func == "t":
-        data = Anime.transfer(args[1:]) if len(args) >= 2 else "Missing parameters (syntax: transfer Name)"
+        data = Anime.transfer(args[1:]) if len(args) >= 2 else Guide.anime_transfer
         await ctx.send(data)
     elif func == "change":
-        data = Anime.change_time(args[1:-1], args[-1]) if len(args) >= 3 else \
-            "Missing parameters (syntax: change Name Hour)"
+        data = Anime.change_time(args[1:-1], args[-1]) if len(args) >= 3 else Guide.anime_change
         await ctx.send(data)
     else:
-        await ctx.send(
-            "Wrong function(available: watched, add, add_going, finished, status, waiting, update, change, transfer)")
+        await ctx.send(Guide.anime_wrong_function)
 
 
 @bot.command()
@@ -175,11 +174,12 @@ async def mention(ctx, name, text, sleep_time):
     Notes.delete_note(name)
 
 
+"""
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Command not found")
     else:
         await ctx.send(str(error))
-
+"""
 bot.run(TOKEN)
